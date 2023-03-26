deb6aadfc20a107bc32b81202cff54b98c480f50
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-03-26 22:18:08 +0300

    Adjust to binary_zero_fill_empty parameter name to be as binary_num_list for more clarity

--- helpers/utils.py
+++ helpers/utils.py
@@ -14,25 +14,25 @@ class Utils:
         pass
 
     @classmethod
-    def binary_zero_fill_empty(cls, binary_num: list) -> str:
+    def binary_zero_fill_empty(cls, binary_num_list: list) -> str:
         """
            Adds zero to autofill binary numbers less than 8 bit.
 
            Args:
-               binary_num (list): list of the ip binary numbers.
+               binary_num_list (list): list of the ip binary numbers.
 
            Returns:
                str: A string representing the binary representation of the IP address After applying zero padding.
            """
         fixed_binary_nums = ""
-        for i in range(len(binary_num)):
-            if len(binary_num.__getitem__(i)) > 8:
+        for i in range(len(binary_num_list)):
+            if len(binary_num_list.__getitem__(i)) > 8:
                 return "The Number You Just Entered is above 8 bit"
-            if len(binary_num.__getitem__(i)) == 8:
-                fixed_binary_nums += f"{binary_num.__getitem__(i)}."
-            if len(binary_num.__getitem__(i)) < 8:
-                autofill_empty_zero = str(8 - len(binary_num.__getitem__(i)))
-                fixed_binary_nums += f"{cls.BINARY_ZEROING.get(autofill_empty_zero)}{binary_num.__getitem__(i)}."
+            if len(binary_num_list.__getitem__(i)) == 8:
+                fixed_binary_nums += f"{binary_num_list.__getitem__(i)}."
+            if len(binary_num_list.__getitem__(i)) < 8:
+                autofill_empty_zero = str(8 - len(binary_num_list.__getitem__(i)))
+                fixed_binary_nums += f"{cls.BINARY_ZEROING.get(autofill_empty_zero)}{binary_num_list.__getitem__(i)}."
         return fixed_binary_nums[:-1]
 
     @staticmethod
